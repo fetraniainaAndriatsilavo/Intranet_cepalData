54fd1fe9af490444e4c4ac7a613e31fc42904af5
Fetraniana A. <andri.fetraniaina@gmail.com>
2025-08-18 16:53:25 +0300

    corr1808

--- Intranet_cepalData/Template/src/components/Authentification/Cover.jsx
+++
@@ -1,6 +0,0 @@
-import image from '../../images/WhatsApp Image 2025-07-24 at 10.55.22_1c56ba4d.jpg';
-export default function Cover() {
-    return <div className="flex flex-col h-full bg-white items-center justify-center w-full">
-            <img alt="Cover" src={image} className="h-full"/>
-    </div>
-}
--- Intranet_cepalData/Template/src/components/Conges/TableHeader.jsx
+++
@@ -1,7 +0,0 @@
-export default function TableHeader({colsName}) {
-  return (
-    <th scope="col" className="px-6 py-3">
-      {colsName}
-    </th>
-  );
-}
